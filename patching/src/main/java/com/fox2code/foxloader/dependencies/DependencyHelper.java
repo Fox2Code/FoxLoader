87eafe666ff441480460e6d3976f37a8eaf0e4cd
Fox2Code <fox2code@gmail.com>
2025-06-07 22:31:26 +0200

    Extends kotlinx libraries set.

--- patching/src/main/java/com/fox2code/foxloader/dependencies/DependencyHelper.java
+++ patching/src/main/java/com/fox2code/foxloader/dependencies/DependencyHelper.java
@@ -143,6 +143,8 @@ public final class DependencyHelper {
                     MAVEN_CENTRAL, "kotlin.KotlinVersion", null, "263bdc679e1f62012db7b091796279b6d71cf36f4797a98ff1ace05835f201c8"),
             new Dependency("org.jetbrains.kotlinx:atomicfu-jvm:" + BuildConfig.KOTLINX_ATOMICFU_VERSION,
                     MAVEN_CENTRAL, "kotlinx.atomicfu.AtomicRef", null, "2b68464170070a8b085d8a7224c7c002dbd65ea14e1f8b97a9605115a252f7fb"),
+            new Dependency("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:" + BuildConfig.KOTLINX_COLLECTIONS_IMMUTABLE_VERSION,
+                    MAVEN_CENTRAL, "kotlinx.collections.immutable.ImmutableCollection", null, "d767014ad0c9a27d27d26fd38e7afa030aee0d141338f108781ff02ecf2fdab5"),
             new Dependency("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:" + BuildConfig.KOTLINX_COROUTINES_VERSION,
                     MAVEN_CENTRAL, "kotlinx.coroutines.CoroutineDispatcher", null, "5ca175b38df331fd64155b35cd8cae1251fa9ee369709b36d42e0a288ccce3fd"),
             new Dependency("org.jetbrains.kotlinx:kotlinx-datetime-jvm:" + BuildConfig.KOTLINX_DATETIME_VERSION,
