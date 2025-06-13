9189b2ebf1503ebf3e939c4c5e9a2427adb6c7a5
Fox2Code <fox2code@gmail.com>
2025-06-13 16:10:53 +0200

    Add hotfix for UTF-8 properties file.

--- loader/src/main/java/com/fox2code/foxloader/internal/InternalHotfixesHooks.java
+++ loader/src/main/java/com/fox2code/foxloader/internal/InternalHotfixesHooks.java
@@ -21,11 +21,18 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.fox2code.foxloader.client.gui;
+package com.fox2code.foxloader.internal;
 
-import net.minecraft.client.gui.GuiScreen;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
-@FunctionalInterface
-public interface GuiConfigProviderConfigObject {
-    GuiScreen provideConfigScreen(GuiScreen parent);
+public class InternalHotfixesHooks {
+    public static void loadProperties(Properties properties, InputStream inputStream) throws IOException {
+        properties.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+    }
+
+    public static void storeProperties(Properties properties, OutputStream outputStream, String comment) throws IOException {
+        properties.store(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8), comment);
+    }
 }
--- patching/src/main/java/com/fox2code/foxloader/patching/game/HotfixesPatch.java
+++ patching/src/main/java/com/fox2code/foxloader/patching/game/HotfixesPatch.java
@@ -23,21 +23,50 @@
  */
 package com.fox2code.foxloader.patching.game;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 
 /**
  * Theses are just hotfixes for the game, dedicated to fixing ReIndev bugs and crashes.
  */
 final class HotfixesPatch extends GamePatch {
-    static final boolean USE_HOTFIXES = false;
+    private static final String PropertyManager = "net/minecraft/server/util/PropertyManager";
+    private static final String Properties = "java/util/Properties";
+    private static final String InternalHotfixesHooks = "com/fox2code/foxloader/internal/InternalHotfixesHooks";
+    static final boolean USE_HOTFIXES = true;
 
     HotfixesPatch() {
-        super(new String[]{});
+        super(new String[]{PropertyManager});
     }
 
     @Override
     public ClassNode transform(ClassNode classNode) {
+        if (PropertyManager.equals(classNode.name)) {
+            patchPropertyManager(classNode);
+        }
         return classNode;
     }
 
+    private static void patchPropertyManager(ClassNode classNode) {
+        for (MethodNode methodNode : classNode.methods) {
+            for (AbstractInsnNode abstractInsnNode : methodNode.instructions) {
+                if (abstractInsnNode.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+                    MethodInsnNode methodInsnNode = (MethodInsnNode) abstractInsnNode;
+                    if (Properties.equals(methodInsnNode.owner)) {
+                        if ("load".equals(methodInsnNode.name) &&
+                                "(Ljava/io/InputStream;)V".equals(methodInsnNode.desc)) {
+                            methodNode.instructions.set(methodInsnNode, new MethodInsnNode(
+                                    Opcodes.INVOKESTATIC, InternalHotfixesHooks, "loadProperties",
+                                    "(L" + Properties + ";Ljava/io/InputStream;)V"));
+                        } else if ("store".equals(methodInsnNode.name) &&
+                                "(Ljava/io/OutputStream;Ljava/lang/String;)V".equals(methodInsnNode.desc)) {
+                            methodNode.instructions.set(methodInsnNode, new MethodInsnNode(
+                                    Opcodes.INVOKESTATIC, InternalHotfixesHooks, "storeProperties",
+                                    "(L" + Properties + ";Ljava/io/OutputStream;Ljava/lang/String;)V"));
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
