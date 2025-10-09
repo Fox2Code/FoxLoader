08910e7f62bcf0d9ec7c073816e850a19fce630e
Fox2Code <fox2code@gmail.com>
2025-10-09 13:17:09 +0200

    Use cleanthat for code formatting.

--- patching/src/main/java/com/fox2code/foxloader/utils/Platform.java
+++ patching/src/main/java/com/fox2code/foxloader/utils/Platform.java
@@ -122,7 +122,7 @@ public enum Platform {
         if(version.startsWith("1.")) {
             version = version.substring(2, 3);
         } else {
-            int dot = version.indexOf(".");
+            int dot = version.indexOf('.');
             if(dot != -1) { version = version.substring(0, dot); }
         } return Integer.parseInt(version);
     }
